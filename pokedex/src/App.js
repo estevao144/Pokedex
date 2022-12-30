6ae4f9c1bd055903a5e6e1da04147f2e63982d95
estevao144 <estevo144@gmail.com>
2022-12-30 15:17:40 -0300

    rota de pagina feita

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Pokedex from "./components/Pokedex";
-import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <Routes>
-      <Route path="/"  element={ Pokedex } />
-    </Routes>
+<Pokedex/>
   );
 }
 
